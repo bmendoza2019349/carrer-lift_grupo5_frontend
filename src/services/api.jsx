1300a4f1b9bb41b1ae6963db9cf8e57a4fa875ec
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-07-18 17:57:50 -0600

    "Formatting changes in Register component, API service, and useRegister hook"

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -10,7 +10,7 @@ apiClient.interceptors.request.use(
     ( config ) => {
         const userDetails = localStorage.getItem( 'user' );
         if ( userDetails ) {
-            const { token } = JSON.parse( userDetails ).token;
+            const token = JSON.parse( userDetails ).token;
             config.headers.Authorization = `Bearer ${token}`;
         }
         return config;
